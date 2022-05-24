713754314432b5519e08bfdd966b1ed7e16c8e65
yuniela <yunielasolorzano@gmail.com>
2022-05-24 08:11:22 -0500

    Agregando responsive desing al home y contacto

--- src/components/ui/Contact.js
+++ src/components/ui/Contact.js
@@ -6,7 +6,7 @@ export const Contact = () => {
   return (
     <div className="container">
         <h1 >Ways To Contact Me...</h1>
-        <div className="banner_img">
+        <div className="banner_img_cont">
           <img src= "./assets/contactme.SVG" className="d-inline-block align-right" alt=""/>
         </div>
         <Form/>
